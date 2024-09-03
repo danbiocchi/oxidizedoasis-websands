f7b1ff08f9c5f6a5a88cb5f276de837f52917ec4
Daniel <danielbiocchi@hotmail.com>
2024-09-02 21:35:51 -0400

    Import changes: Removed the duplicate import of Method from the external http crate. Now using actix_web::http::Method consistently throughout the file. 2. Enhanced logging: Added more detailed logging for both CORS and non-CORS requests. Now logging the HTTP method for all requests. Added separate logging for preflight (OPTIONS) requests. Included logging for additional CORS headers: Access-Control-Allow-Origin Access-Control-Allow-Methods Access-Control-Allow-Headers Improved conditional logging: Using info! level for main CORS request and response information. Using debug! level for additional details like preflight requests and specific CORS headers. Added a debug! log for non-CORS requests. Code structure: The overall structure of the middleware remains the same, but with enhanced logging capabilities.

--- src/middleware/cors_logger.rs
+++ src/middleware/cors_logger.rs
@@ -1,9 +1,10 @@
 use actix_web::dev::{Service, ServiceRequest, ServiceResponse, Transform};
 use actix_web::Error;
+use actix_web::http::Method;  // Use http types from actix_web
 use futures::future::{ok, Ready};
-use futures::Future;
-use log::info;
+use std::future::Future;
 use std::pin::Pin;
+use log::{debug, info};
 
 pub struct CorsLogger;
 
@@ -15,8 +16,8 @@ where
 {
     type Response = ServiceResponse<B>;
     type Error = Error;
-    type Transform = CorsLoggerMiddleware<S>;
     type InitError = ();
+    type Transform = CorsLoggerMiddleware<S>;
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
@@ -45,20 +46,29 @@ where
     fn call(&self, req: ServiceRequest) -> Self::Future {
         let origin = req.headers().get("Origin").and_then(|h| h.to_str().ok()).map(|s| s.to_owned());
         let is_cors = origin.is_some();
+        let method = req.method().clone();
 
         let fut = self.service.call(req);
 
         Box::pin(async move {
             let res = fut.await?;
-
             if is_cors {
-                info!(
-                    "CORS request: Origin: {:?}, Status: {}",
-                    origin,
-                    res.status()
-                );
+                info!("CORS request: Method: {:?}, Origin: {:?}", method, origin);
+                if method == Method::OPTIONS {
+                    debug!("CORS preflight request");
+                }
+                if let Some(allow_origin) = res.headers().get("Access-Control-Allow-Origin") {
+                    info!("CORS response: Access-Control-Allow-Origin: {:?}", allow_origin);
+                }
+                if let Some(allow_methods) = res.headers().get("Access-Control-Allow-Methods") {
+                    debug!("CORS response: Access-Control-Allow-Methods: {:?}", allow_methods);
+                }
+                if let Some(allow_headers) = res.headers().get("Access-Control-Allow-Headers") {
+                    debug!("CORS response: Access-Control-Allow-Headers: {:?}", allow_headers);
+                }
+            } else {
+                debug!("Non-CORS request: Method: {:?}", method);
             }
-
             Ok(res)
         })
     }
