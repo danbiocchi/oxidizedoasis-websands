75782e0eb7972ab571ee93a784d8ecf22f60c0c0
Daniel <danielbiocchi@hotmail.com>
2025-02-05 23:00:40 -0500

    logout button

--- frontend/src/components/nav.rs
+++ frontend/src/components/nav.rs
@@ -60,7 +60,7 @@ pub fn nav() -> Html {
                 </div>
                 <div class="c-navbar__actions">
                     if auth_context.is_authenticated {
-                        <button onclick={logout} class="c-button c-button--error">
+                        <button onclick={logout} class="c-button c-button--outline-error">
                             { "Logout" }
                         </button>
                     } else {
